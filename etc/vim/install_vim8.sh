754655a1c5b8eab6ce132f84b62c016bac5834e4
Tiryoh <tiryoh@gmail.com>
2018-06-30 16:22:49 +0900

    Fix "add-apt-repository" not found error
    Thanks to @masamasa9841

--- etc/vim/install_vim8.sh
+++ etc/vim/install_vim8.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -eu
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:jonathonf/vim
 sudo apt-get update
-sudo apt-get install vim
+sudo apt-get install -y vim
