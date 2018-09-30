6781eee2f96608345e234f2abf57dde753b8d9ec
Tiryoh <tiryoh@gmail.com>
2018-10-01 01:20:06 +0900

    Fix "File exists" error

--- etc/bash/bash_completion-docker.sh
+++ etc/bash/bash_completion-docker.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -eu
-mkdir ~/.bash_completion.d
+mkdir -p ~/.bash_completion.d
 curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o ~/.bash_completion.d/docker-compose
 curl -L https://raw.githubusercontent.com/docker/cli/$(docker version --format 'v{{.Server.Version}}')/contrib/completion/bash/docker -o ~/.bash_completion.d/docker
-
